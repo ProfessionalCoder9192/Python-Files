be571210a6f622abdc5909ce8c573196027a23a7
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-03-11 18:03:04 -0400

    Add files via upload

---
+++ DataTypes.py
@@ -0,0 +1,9 @@
+Name="Anchit"
+Age=11
+Weight=53.49
+boolh=True
+
+print("The data type of name is ",type(Name))
+print("The data type of age is ",type(Age))
+print("The data type of weight is ",type(Weight))
+print("The data type of boolh is ",type(boolh))
