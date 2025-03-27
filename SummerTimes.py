60d446bfbc74db7cfe8209b4c0aa148b1a7eb510
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-03-27 17:46:02 -0400

    Add files via upload

---
+++ SummerTimes.py
@@ -0,0 +1,5 @@
+Temp = int(input("What temperature is it outside:"))
+if Temp > 50:
+    print ("This is suitable weather for light clothing.")
+else:
+    print ("It is too cold for light clothing.")
