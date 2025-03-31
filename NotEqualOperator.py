de9ef44dfffd928367a841aad0e8e05f410910a5
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-03-31 17:41:33 -0400

    Add files via upload

---
+++ NotEqualOperator.py
@@ -0,0 +1,20 @@
+a = 28
+b = 28
+c = 34
+d = "Math"
+e = "English"
+f = int(input("Enter a number:"))
+
+print (a != b)
+print (b != c)
+
+if d != e:
+    print (d, "and", e, "are different.")
+
+if (a == 30) != (b == 28):
+    print (a, "and", b, "are not equal.")
+
+if a%2 != 0:
+    print (a, "is not an even number.")
+
+
