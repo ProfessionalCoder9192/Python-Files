e2ebb07903ea5e3128731609fb062e0fa2ec7eef
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-04-18 16:46:40 -0400

    Add files via upload

---
+++ Bitwise.py
@@ -0,0 +1,13 @@
+a = 20
+b = -10
+
+# print bitwise right shift operator
+print ("a >> 1 =", a >> 1)
+print ("b >> 1 =", b >> 1)
+
+a = 15
+b = -20
+
+# print bitwise left shift operator
+print ("a << 1 =", a << 1)
+print ("b << 1 =", b << 1)
