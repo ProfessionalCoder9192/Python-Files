908fb0b9c02586f6ad82001f49c50ad432fe3408
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-04-22 17:32:52 -0400

    Add files via upload

---
+++ OperatorPrecedence.py
@@ -0,0 +1,15 @@
+v = 8
+w = 6
+x = 3
+y = 10
+z = 18
+a = (x + w)/x*v
+print("The value of (x+w)/x*v is", a)
+
+name = "Anchit" 
+age = 11
+
+if name == "Anchi" or name == "John" and age >=10:
+    print("Hello!")
+else:
+    print("Error!")
