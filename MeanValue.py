c7b71b4c26716bd24280ea594c1bc980846b4714
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-04-22 17:48:34 -0400

    Add files via upload

---
+++ MeanValue.py
@@ -0,0 +1,8 @@
+mean1 = 59
+wrongnumber = 38
+correctnumber = 73
+numberstotal = mean1*40
+print("The mean of the numbers is", numberstotal)
+print("One of the numbers were incorrect, it was", wrongnumber, "instead of", correctnumber)
+newtotal = (numberstotal-38)+73
+print("The new total of the numbers is", newtotal)
