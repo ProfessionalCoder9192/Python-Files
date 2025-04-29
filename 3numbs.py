c2eae0e0a3a74722f15ba92736acdfbd3ff353a2
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-04-29 16:03:01 -0400

    Add files via upload

---
+++ 3numbs.py
@@ -0,0 +1,9 @@
+print("The current answer to the problem is 1700 but 3 numbers are wrong.")
+wrong1=86
+wrong2=98
+wrong3=98
+correct1=92
+correct2=78
+correct3=102
+newtotal= (1700-wrong1-wrong2-wrong3)+correct1+correct2+correct3
+print("The new total is", newtotal)
