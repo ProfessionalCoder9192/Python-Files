adf1bcefdc48e135ff13a32f2dcac64b0026ae33
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-05-20 18:39:58 -0400

    Add files via upload

---
+++ Star.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.Screen().bgcolor("Green")
+board = turtle.Turtle()
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+board.pendown()
+
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(100)
