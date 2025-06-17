48d943007ce5620d33aec66030e11c1be3dce2cf
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-06-17 17:15:41 -0400

    Add files via upload

---
+++ change.py
@@ -0,0 +1,11 @@
+
+item_price = (input("Whats the item price?"))
+
+
+amount_given = (input("Whats the amount given?:"))
+
+
+change = amount_given - item_price
+
+
+print("Change to return: $", change)
