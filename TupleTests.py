728d5bfd037bebdbcdd8d98135a2be57353bc16e
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-08-26 17:43:21 -0400

    Add files via upload

---
+++ TupleTests.py
@@ -0,0 +1,16 @@
+TupleA = (True, "Hello", 2.8, 9)
+print (TupleA)
+
+TupleB = (6, 9, 2, 3, 6, 0, 1, 4)
+print (TupleB)
+TupleB = TupleB + (5,)
+print (TupleB)
+
+TupleC = (50, 40, 50, 20, 70, 50, 80)
+print (TupleC.count(50))
+
+TupleD = ("P", "Y", "T", "H", "O", "N")
+Sl = TupleD[1:4]
+print (Sl)
+Sl = TupleD[:5]
+print (Sl)
