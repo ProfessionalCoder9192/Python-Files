c46b6f5c1ce33a3c6821ca73eec7e4915b390b19
ProfessionalCoder9192 <anchitshankar@gmail.com>
2025-09-09 17:52:30 -0400

    Add files via upload

--- arrtest.py
+++ arrtest.py
@@ -3,7 +3,8 @@ import array as arr
 array_num = arr.array('i', [1, 3, 5, 3, 7, 9, 3])
 print("Original array: "+str(array_num))
 
-print("Number of occurrences of the number 3 in the said array: +str(array_num.count(3))")
+print("Number of occurrences of the number 3 in the said array:"
++str(array_num.count(3)))
 
 array_num.reverse()
 print("Reverse the order of the items:")
