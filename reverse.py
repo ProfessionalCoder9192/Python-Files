75d20a5e1e18a7b1d0e4c3e0bf5d3d128d281561
ProfessionalCoder9192 <anchitshankar@gmail.com>
2026-03-03 11:09:12 -0500

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,11 @@
+class Reverse:
+    def __init__(self, s="s"):
+        self.s = s
+
+    def reverse_string(self):
+        return self.s[::-1]
+
+
+user_input = input("Enter a word: ")
+obj = Reverse(user_input)
+print("Reversed string:", obj.reverse_string())
