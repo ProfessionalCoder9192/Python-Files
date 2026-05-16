eed8d32e8b83223402bef1ae27b2e9c32c980674
ProfessionalCoder9192 <anchitshankar@gmail.com>
2026-05-16 18:40:53 -0400

    Add files via upload

---
+++ HelloAI.py
@@ -0,0 +1,14 @@
+print('Hello I am an AI bot. Whats your name?:')
+name=input()
+print(f'Nice to meet you, {name}!')
+print('How are you feeling today?(Good/Bad):')
+mood=input().lower()
+
+if mood=='good':
+    print("I am glad to hear that!")
+elif mood == 'bad':
+    print("I am sorry to hear that. Hope things get better soon.")
+else:
+    print("I see. Sometimes it's hard to put feelings into words.")
+
+print(f'It was nice chatting with you, {name}! Goodbye!')
